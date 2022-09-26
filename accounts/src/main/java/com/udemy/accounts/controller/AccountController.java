d9e18fa5e85046f9e6bde3cc170a85aaabda3b9f
Tihomir Kehayov <kehayov89@gmail.com>
2022-09-26 09:55:55 +0300

    add

--- accounts/src/main/java/com/udemy/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/udemy/accounts/controller/AccountController.java
@@ -1,6 +1,12 @@
 package com.udemy.accounts.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.udemy.accounts.AccountsServiceConfig;
+import com.udemy.accounts.com.udemy.model.Properties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +16,11 @@ public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private AccountsServiceConfig accountsConfig;
+
     @PostMapping("/myAccount")
-    public Accounts getAccountDetails(@RequestBody Customer customer ){
+    public Accounts getAccountDetails(@RequestBody Customer customer) {
 
         Accounts accounts = accountRepository.findByCustomerId(customer.getCustomerId());
 
@@ -21,4 +30,12 @@ public class AccountController {
         return null;
     }
 
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+                accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
+    }
 }
